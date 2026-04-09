dce2eee0d53170034b80e729315c3efc7b1f4f0b
Dina Muravska <dina.muravska.25@gmail.com>
2026-04-09 18:53:29 +0300

    fix developer name

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -6,10 +6,10 @@ export default function Footer() {
       <div className={css.content}>
         <p>© {new Date().getFullYear()} NoteHub. All rights reserved.</p>
         <div className={css.wrap}>
-          <p>Developer: your name</p>
+          <p>Developer: Dina Muravska</p>
           <p>
             Contact us:
-            <a href="mailto:student@notehub.app">student@notehub.app</a>
+            <a href="dina.muravska.25@gmail.com">dina.muravska.25@gmail.com</a>
           </p>
         </div>
       </div>
